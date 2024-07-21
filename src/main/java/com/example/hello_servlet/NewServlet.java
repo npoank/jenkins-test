5d650a544d9fa5ea00b080b3ed5379c2fbfc684c
npoank <kubl-ml@mail.ru>
2024-07-21 14:07:15 +0300

    first

---
+++ src/main/java/com/example/hello_servlet/NewServlet.java
@@ -0,0 +1,27 @@
+package com.example.hello_servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "NewServlet", value = "/hello-servlet")
+public class NewServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter pw = response.getWriter();
+
+        pw.println("<html>");
+        pw.println("<h1> Hello world Java </h1>");
+        pw.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
